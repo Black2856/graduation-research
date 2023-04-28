130064c283bf65f726431f5ef1a004e14917e889
black2856 <naoki2856@gmail.com>
2023-04-28 15:09:22 +0900

    test

---
+++ 研究用.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+class Application(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        #self.overrideredirect(True)  # タイトルバーを非表示にする
+        self.title("Transparent window")
+        self.geometry("500x300")
+        self.config(bg="white")
+        self.attributes("-transparentcolor", "white")
+        self.attributes('-fullscreen', True)
+
+        canvas = tk.Canvas(self, width=self.winfo_screenwidth(), height=self.winfo_screenheight(), bg="white")
+        canvas.configure(background='red')
+        canvas.pack()
+
+        canvas.create_text(600, 250, text="エラーコード 0x800F081F：Windows更新プログラムのインストール中に問題が発生しました。", font=("Arial", 60), fill="blue")
+if __name__ == "__main__":
+    application = Application()
+    application.mainloop()
+
+
+
+    #Ik3awI_yeKCSI
